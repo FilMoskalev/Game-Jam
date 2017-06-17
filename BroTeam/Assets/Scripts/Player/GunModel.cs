c75c14edab25cc90851aa9ebe997662b075ad13d
Maks <m.verteb@plarium.com>
2017-06-17 11:08:29 +0300

    addfirstGun

---
+++ BroTeam/Assets/Scripts/Player/GunModel.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Player
+{
+    public class GunModel {
+
+        public int SpeedRotation { get; set; }
+        public int Force { get; set; }
+        public GameObject TypeWeapon { get; set; }
+        public int GunNumber { get; set; }
+    }
+}
---
+++ BroTeam/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Player
+{
+	public class PlayerController : MonoBehaviour
+	{
+
+		private Rigidbody _rbPlayer;
+		private GameObject _firstGun;
+
+		private GunModel[] _gunModels;
+		
+		void Start ()
+		{
+			_rbPlayer = GetComponent<Rigidbody>();
+			_firstGun = GameObject.FindWithTag("FirstGun");
+		}
+	
+		void Update () {
+			
+		}
+
+		void FixedUpdate()
+		{
+			var inputFirstPlayer = Input.GetAxis("Horizontal");
+			_firstGun.transform.rotation *= Quaternion.Euler(0f, 0f, inputFirstPlayer*Time.deltaTime);
+		}
+	}
+}
